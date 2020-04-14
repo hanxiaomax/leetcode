2e8d69738741bf63b3e82cf42e353df1fead970c
hanxiaomax-mac <hanxiaomax@qq.com>
2020-04-14 09:57:56 +0800

    init

---
+++ 200_number-of-islands.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=200 lang=python3
+#
+# [200] 岛屿数量
+#
+# https://leetcode-cn.com/problems/number-of-islands/description/
+#
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+# @lc code=end
+
