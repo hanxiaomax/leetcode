cd0f20b1ef78765fc71888eff610f07185485dbe
hanxiaomax-mac <hanxiaomax@qq.com>
2020-04-18 10:09:29 +0800

    submit #21 mergesort and recursion

---
+++ 21_merge-two-sorted-lists.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode.cn id=21 lang=python3
+#
+# [21] 合并两个有序链表
+#
+# https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+#
+# algorithms
+# Easy (60.98%)
+
+#
+# 将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+# 
+# 示例：
+# 
+# 输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+# 
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) :
+        
+        pre_head = ListNode(0)
+        prev = pre_head
+
+        #遍历链表并比较两个链表中的当前元素，将较小的元素链接到新链表中
+        #如果从该链表中取了元素，则该链表头指针后移
+        #哨兵链表的节点每次均向后移动因为必然有新的节点加入
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        # 收集剩余的链表
+        prev.next = l1 if l1 is not None else l2
+
+        return pre_head.next #从哨兵后第一个节点返回
+# @lc code=end
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
