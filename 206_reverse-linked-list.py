3f5702d6de675c52b4d0e6cbb20b013ca08fac63
hanxiaomax-mac <hanxiaomax@qq.com>
2020-04-18 21:11:33 +0800

    submit #206

---
+++ 206_reverse-linked-list.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=206 lang=python3
+#
+# [206] 反转链表
+#
+# https://leetcode-cn.com/problems/reverse-linked-list/description/
+#
+# algorithms
+# Easy (68.45%)
+
+# 反转一个单链表。
+# 
+# 示例:
+# 
+# 输入: 1->2->3->4->5->NULL
+# 输出: 5->4->3->2->1->NULL
+# 
+# 进阶:
+# 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+# @lc code=end
+
